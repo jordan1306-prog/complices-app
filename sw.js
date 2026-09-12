c6fec7c752cfe7f89988ecc307901ec3124d0c96
jordan1306-prog <jordan1306@hotmail.fr>
2026-09-12 21:15:58 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'complices-v26';
+const CACHE_NAME = 'complices-v27';
 const ASSETS = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', event => {
