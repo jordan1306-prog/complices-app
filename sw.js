06207fdc31b89c8d1c5dabcafa2acd112865faa1
jordan1306-prog <jordan1306@hotmail.fr>
2026-09-15 18:55:49 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 importScripts('https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js');
 
-const CACHE_NAME = 'complices-v56';
+const CACHE_NAME = 'complices-v57';
 const ASSETS = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png', './background.jpg'];
 
 self.addEventListener('install', event => {
